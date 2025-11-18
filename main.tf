8f3e155a968c264b51f8229fa20f65fadbbc3d24
nponneboyina77 <nponneboyina@gmail.com>
2025-11-18 09:56:00 +0530

    Update instance type and key name in main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_launch_template" "web_server_as" {
     name = "myproject"
     image_id           = "ami-03ff9720b16e8da76"
     vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "t3.micro"
-    key_name = "minimajor"
+    instance_type = "t2.micro"
+    key_name = "narendra"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-00415324c5e8c9e81", "subnet-0a16acd6f33ac0422"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
